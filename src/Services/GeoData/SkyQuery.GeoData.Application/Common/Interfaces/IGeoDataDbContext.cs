51f1095ae3488a11f0e2ba8fb1bc1fb30d9c21ba
Erdem <erdemsincer123@icloud.com>
2025-06-11 02:36:12 +0300

    extension

---
+++ src/Services/GeoData/SkyQuery.GeoData.Application/Common/Interfaces/IGeoDataDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SkyQuery.GeoData.Domain.Entities;
+using System.Collections.Generic;
+
+namespace SkyQuery.GeoData.Application.Common.Interfaces;
+
+public interface IGeoDataDbContext
+{
+    DbSet<Domain.Entities.GeoData> GeoDatas { get; }
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
