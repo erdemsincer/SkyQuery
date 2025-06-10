4d891bacf76b87c9576bcbb5712d5481a036aaae
Erdem <erdemsincer123@icloud.com>
2025-06-11 02:15:39 +0300

    GET ALL LOCATİON

---
+++ src/Services/Location/SkyQuery.Location.Application/Locations/Queries/GetAllLocationsQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using SkyQuery.Location.Domain.Entities;
+
+namespace SkyQuery.Location.Application.Locations.Queries;
+
+public class GetAllLocationsQuery : IRequest<List<Domain.Entities.Location>> { }
---
+++ src/Services/Location/SkyQuery.Location.Application/Locations/Queries/GetAllLocationsQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SkyQuery.Location.Application.Common.Interfaces;
+using SkyQuery.Location.Domain.Entities;
+
+namespace SkyQuery.Location.Application.Locations.Queries;
+
+public class GetAllLocationsQueryHandler : IRequestHandler<GetAllLocationsQuery, List<Domain.Entities.Location>>
+{
+    private readonly ILocationDbContext _context;
+
+    public GetAllLocationsQueryHandler(ILocationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Domain.Entities.Location>> Handle(GetAllLocationsQuery request, CancellationToken cancellationToken)
+    {
+        return await _context.Locations.ToListAsync(cancellationToken);
+    }
+}
