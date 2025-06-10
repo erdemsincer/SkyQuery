9bba04a9c20b2e8980ad18972a77e35cae7bb4d1
Erdem <erdemsincer123@icloud.com>
2025-06-11 02:35:30 +0300

    geodata entity

---
+++ src/Services/GeoData/SkyQuery.GeoData.Domain/Entities/GeoData.cs
@@ -0,0 +1,12 @@
+﻿namespace SkyQuery.GeoData.Domain.Entities;
+
+public class GeoData
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public string Climate { get; set; } = string.Empty;
+    public string Vegetation { get; set; } = string.Empty;
+    public string Elevation { get; set; } = string.Empty;
+    public string Summary { get; set; } = string.Empty; // AI'dan gelen özet
+}
