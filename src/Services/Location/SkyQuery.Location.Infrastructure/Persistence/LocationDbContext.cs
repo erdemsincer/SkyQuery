b2dd25eed2453f134d80a534d6e87a9872676df6
Erdem <erdemsincer123@icloud.com>
2025-06-11 02:15:59 +0300

    CONTEX

---
+++ src/Services/Location/SkyQuery.Location.Infrastructure/Persistence/LocationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SkyQuery.Location.Application.Common.Interfaces;
+using SkyQuery.Location.Domain.Entities;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace SkyQuery.Location.Infrastructure.Persistence;
+
+public class LocationDbContext : DbContext, ILocationDbContext
+{
+    public LocationDbContext(DbContextOptions<LocationDbContext> options) : base(options) { }
+    public DbSet<Domain.Entities.Location> Locations => Set<Domain.Entities.Location>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Domain.Entities.Location>().ToTable("Locations");
+    }
+}
