6e4f93c53dafe41614f85d39d0779c836306f655
Erdem <erdemsincer123@icloud.com>
2025-06-11 02:14:01 +0300

    USER REPOSİTORY

--- src/Services/Auth/SkyQuery.Auth.Infrastructure/Persistence/UserRepository.cs
+++ src/Services/Auth/SkyQuery.Auth.Infrastructure/Persistence/UserRepository.cs
@@ -17,4 +17,7 @@ public class UserRepository : IUserRepository
         _context.Users.Add(user);
         await _context.SaveChangesAsync();
     }
+    public async Task<User?> GetByEmailAsync(string email)
+    => await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+
 }
