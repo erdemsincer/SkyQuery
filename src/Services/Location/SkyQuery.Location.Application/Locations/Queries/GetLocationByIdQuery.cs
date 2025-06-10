44e06e3a2da982a859a3de765d0e63ac7ce54407
Erdem <erdemsincer123@icloud.com>
2025-06-11 02:15:46 +0300

    GETBYID LCOATİON

---
+++ src/Services/Location/SkyQuery.Location.Application/Locations/Queries/GetLocationByIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using SkyQuery.Location.Domain.Entities;
+
+namespace SkyQuery.Location.Application.Locations.Queries;
+
+public class GetLocationByIdQuery : IRequest<Domain.Entities.Location?>
+{
+    public Guid Id { get; set; }
+
+    public GetLocationByIdQuery(Guid id)
+    {
+        Id = id;
+    }
+}
---
+++ src/Services/Location/SkyQuery.Location.Application/Locations/Queries/GetLocationByIdQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SkyQuery.Location.Application.Common.Interfaces;
+using SkyQuery.Location.Domain.Entities;
+
+namespace SkyQuery.Location.Application.Locations.Queries;
+
+public class GetLocationByIdQueryHandler : IRequestHandler<GetLocationByIdQuery, Domain.Entities.Location?>
+{
+    private readonly ILocationDbContext _context;
+
+    public GetLocationByIdQueryHandler(ILocationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Domain.Entities.Location?> Handle(GetLocationByIdQuery request, CancellationToken cancellationToken)
+    {
+        return await _context.Locations
+            .FirstOrDefaultAsync(l => l.Id == request.Id, cancellationToken);
+    }
+}
