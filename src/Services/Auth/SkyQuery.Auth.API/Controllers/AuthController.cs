8480043bc803e4bb5b870aecf387ad7c0876eebd
Erdem <erdemsincer123@icloud.com>
2025-06-10 20:54:18 +0300

    auth controller

---
+++ src/Services/Auth/SkyQuery.Auth.API/Controllers/AuthController.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SkyQuery.Auth.Application.Auth.Commands;
+using SkyQuery.Auth.Contracts.Requests;
+using SkyQuery.Auth.Contracts.Responses;
+
+namespace SkyQuery.Auth.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    public AuthController(IMediator mediator) => _mediator = mediator;
+
+    [HttpPost("register")]
+    public async Task<ActionResult<RegisterUserResponse>> Register(RegisterUserRequest request)
+    {
+        var command = new RegisterUserCommand(request.Email, request.Password);
+        var result = await _mediator.Send(command);
+        return Ok(result);
+    }
+}
--- src/Services/Auth/SkyQuery.Auth.API/Program.cs
+++ src/Services/Auth/SkyQuery.Auth.API/Program.cs
@@ -1,23 +1,45 @@
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using SkyQuery.Auth.Application;
+using SkyQuery.Auth.Infrastructure.Extensions;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// DI
+builder.Services.AddApplication(); // MediatR + Validators
+builder.Services.AddInfrastructure(builder.Configuration); // DB + JWT
 
+// Auth
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        var key = Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!);
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidateAudience = true,
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key)
+        };
+    });
+
+builder.Services.AddAuthorization();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddFluentValidationAutoValidation();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
