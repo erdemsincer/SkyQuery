20e3d2d8ade679a92b2984173a0dd428a29dfa5f
Erdem <erdemsincer123@icloud.com>
2025-06-11 02:16:11 +0300

    EXTENSİONLAR

---
+++ src/Services/Location/SkyQuery.Location.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SkyQuery.Location.Application.Common.Interfaces;
+using SkyQuery.Location.Infrastructure.Persistence;
+
+namespace SkyQuery.Location.Infrastructure.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<LocationDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("LocationDb")));
+
+        services.AddScoped<ILocationDbContext>(provider => provider.GetRequiredService<LocationDbContext>());
+
+        return services;
+    }
+}
