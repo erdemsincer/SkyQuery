d387670ce3aa9e55fcad47b2e6aa68fd509d6002
Erdem <erdemsincer123@icloud.com>
2025-06-10 20:54:25 +0300

    auth işlemi

---
+++ src/Services/Auth/SkyQuery.Auth.Application/Common/Interfaces/IJwtTokenGenerator.cs
@@ -0,0 +1,8 @@
+﻿using SkyQuery.Auth.Domain.Entities;
+
+namespace SkyQuery.Auth.Application.Common.Interfaces;
+
+public interface IJwtTokenGenerator
+{
+    string GenerateToken(User user);
+}
---
+++ src/Services/Auth/SkyQuery.Auth.Application/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using SkyQuery.Auth.Application.Auth.Commands;
+
+namespace SkyQuery.Auth.Application
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(RegisterUserCommand).Assembly));
+            services.AddValidatorsFromAssembly(typeof(RegisterUserCommand).Assembly);
+            return services;
+        }
+    }
+
+}
