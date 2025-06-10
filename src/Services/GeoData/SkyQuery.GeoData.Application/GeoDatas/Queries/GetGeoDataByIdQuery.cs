cb5d970553b290bc7f8841eb85219fb868f170a2
Erdem <erdemsincer123@icloud.com>
2025-06-11 02:36:07 +0300

    queries

---
+++ src/Services/GeoData/SkyQuery.GeoData.Application/GeoDatas/Queries/GetGeoDataByIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using SkyQuery.GeoData.Domain.Entities;
+
+namespace SkyQuery.GeoData.Application.GeoDatas.Queries;
+
+public class GetGeoDataByIdQuery : IRequest<Domain.Entities.GeoData?>
+{
+    public Guid Id { get; set; }
+
+    public GetGeoDataByIdQuery(Guid id)
+    {
+        Id = id;
+    }
+}
---
+++ src/Services/GeoData/SkyQuery.GeoData.Application/GeoDatas/Queries/GetGeoDataByIdQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SkyQuery.GeoData.Application.Common.Interfaces;
+using SkyQuery.GeoData.Domain.Entities;
+
+namespace SkyQuery.GeoData.Application.GeoDatas.Queries;
+
+public class GetGeoDataByIdQueryHandler : IRequestHandler<GetGeoDataByIdQuery, Domain.Entities.GeoData?>
+{
+    private readonly IGeoDataDbContext _context;
+
+    public GetGeoDataByIdQueryHandler(IGeoDataDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Domain.Entities.GeoData?> Handle(GetGeoDataByIdQuery request, CancellationToken cancellationToken)
+    {
+        return await _context.GeoDatas.FirstOrDefaultAsync(g => g.Id == request.Id, cancellationToken);
+    }
+}
