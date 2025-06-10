879a317a8dca4edb9683e861af13c9665678ff8a
Erdem <erdemsincer123@icloud.com>
2025-06-11 02:35:52 +0300

    extensions

---
+++ src/Services/GeoData/SkyQuery.GeoData.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using SkyQuery.GeoData.Application.Common.Interfaces;
+using SkyQuery.GeoData.Infrastructure.Persistence;
+
+namespace SkyQuery.GeoData.Infrastructure.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<GeoDataDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("GeoDataDb")));
+
+        services.AddScoped<IGeoDataDbContext>(provider => provider.GetRequiredService<GeoDataDbContext>());
+
+        return services;
+    }
+}
