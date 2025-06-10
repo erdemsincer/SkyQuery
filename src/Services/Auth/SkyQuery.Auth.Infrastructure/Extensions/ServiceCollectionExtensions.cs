e5e3d36594b5563e0c59310be85554b6e4e8d8c6
Erdem <erdemsincer123@icloud.com>
2025-06-10 20:53:30 +0300

    Service collection

---
+++ src/Services/Auth/SkyQuery.Auth.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SkyQuery.Auth.Application.Auth.Interfaces;
+using SkyQuery.Auth.Application.Common.Interfaces;
+using SkyQuery.Auth.Infrastructure.Identity;
+using SkyQuery.Auth.Infrastructure.Persistence;
+
+namespace SkyQuery.Auth.Infrastructure.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddDbContext<AuthDbContext>(options =>
+            options.UseNpgsql(config.GetConnectionString("AuthDb")));
+
+        services.AddScoped<IAuthDbContext>(provider => provider.GetRequiredService<AuthDbContext>());
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
+
+        return services;
+    }
+}
