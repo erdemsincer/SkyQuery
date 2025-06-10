db2e2596c85fc9cb0a03f96aae5894aba323119e
Erdem <erdemsincer123@icloud.com>
2025-06-11 02:15:23 +0300

    ILOCATİONDBCONTEX

---
+++ src/Services/Location/SkyQuery.Location.Application/Common/Interfaces/ILocationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SkyQuery.Location.Domain.Entities;
+using System.Collections.Generic;
+
+namespace SkyQuery.Location.Application.Common.Interfaces;
+
+public interface ILocationDbContext
+{
+    DbSet<Domain.Entities.Location> Locations { get; }
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
