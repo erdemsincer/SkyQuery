dd57f28ad505d497c5395fe7757f5d4bb86f29b0
Erdem <erdemsincer123@icloud.com>
2025-06-11 02:35:41 +0300

    context

---
+++ src/Services/GeoData/SkyQuery.GeoData.Infrastructure/Persistence/GeoDataDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SkyQuery.GeoData.Application.Common.Interfaces;
+using SkyQuery.GeoData.Domain.Entities;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace SkyQuery.GeoData.Infrastructure.Persistence;
+
+public class GeoDataDbContext : DbContext, IGeoDataDbContext
+{
+    public GeoDataDbContext(DbContextOptions<GeoDataDbContext> options) : base(options) { }
+    public DbSet<Domain.Entities.GeoData> GeoDatas => Set<Domain.Entities.GeoData>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Domain.Entities.GeoData>().ToTable("GeoDatas");
+    }
+}
