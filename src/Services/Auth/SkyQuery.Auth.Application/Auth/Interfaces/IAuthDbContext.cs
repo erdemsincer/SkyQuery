727f207b0a25d112625de16ef6cfbe01f00cd3cd
Erdem <erdemsincer123@icloud.com>
2025-06-10 20:53:58 +0300

    ınterfacesler

---
+++ src/Services/Auth/SkyQuery.Auth.Application/Auth/Interfaces/IAuthDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SkyQuery.Auth.Domain.Entities;
+using System.Collections.Generic;
+
+namespace SkyQuery.Auth.Application.Common.Interfaces;
+
+public interface IAuthDbContext
+{
+    DbSet<User> Users { get; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
---
+++ src/Services/Auth/SkyQuery.Auth.Application/Auth/Interfaces/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using SkyQuery.Auth.Domain.Entities;
+
+namespace SkyQuery.Auth.Application.Auth.Interfaces;
+
+public interface IUserRepository
+{
+    Task<bool> ExistsByEmailAsync(string email);
+    Task AddAsync(User user);
+}
