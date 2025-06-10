1ac7d068e63f8c4e53a0da4515767e4c6e6ca96c
Erdem <erdemsincer123@icloud.com>
2025-06-11 02:15:12 +0300

    LOCATİON ENTİTY

---
+++ src/Services/Location/SkyQuery.Location.Domain/Entities/Location.cs
@@ -0,0 +1,10 @@
+﻿namespace SkyQuery.Location.Domain.Entities;
+
+public class Location
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Country { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+}
